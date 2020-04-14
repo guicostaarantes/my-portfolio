d96ea2edb13fe2bbe72b834cf24a8384fc085146
guicostaarantes <arantes.g@outlook.com>
2020-04-14 18:38:12 -0300

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Bubble1 from "../components/bubble1";
+import bubble1 from "../assets/bubble1.svg";
+
+function Home() {
+  return (
+    <div>
+      <Bubble1 src={bubble1} />
+    </div>
+  );
+}
+
+export default Home;
